2da61c997586c3601c1d6d577120734447af4f27
David Munro <dbhmunro@gmail.com>
2020-04-17 19:14:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,6 +11,10 @@ def sort_array_char_count(array)
   array.sort { |a, b| a.length<=>b.length }
 end
 
-def swap_elements(array, index, destination_index)
+def swap_elements(array)
   array.sort { |a, b| (a.index==1)<=>(b.index==2)}
 end
+
+# def swap_elements(array, index, destination_index)
+#   array.sort { |a, b| (a.index==1)<=>(b.index==2)}
+# end
